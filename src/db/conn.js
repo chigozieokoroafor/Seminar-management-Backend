7c174d1b19704539af51258de651328d2774d1ca
okoroaforchigozie <okoroaforc14@gmail.com>
2024-12-30 19:15:26 +0100

    db connetion fix

--- src/db/conn.js
+++ src/db/conn.js
@@ -1,14 +1,15 @@
+require("dotenv").config()
 const { Sequelize } = require("sequelize")
 const { createPool } = require("mysql2")
 
 const isDevelopment = process.env.DEVELOPMENT == "true";
 
 const conn_option = {
-    database: isDevelopment ? process.env.DB_NAME : process.env.MYSQL_ADDON_DB,
-    username: isDevelopment ? process.env.DB_USER : process.env.MYSQL_ADDON_USER,
-    password: isDevelopment ? process.env.DB_PWD : process.env.MYSQL_ADDON_PASSWORD,
-    host: isDevelopment ? process.env.DB_HOST : process.env.MYSQL_ADDON_HOST,
-    port: isDevelopment ? process.env.DB_PORT : process.env.MYSQL_ADDON_PORT,
+    database: process.env.DB_NAME ,
+    username: process.env.DB_USER ,
+    password: process.env.DB_PWD ,
+    host: process.env.DB_HOST ,
+    port: process.env.DB_PORT ,
     dialect: 'mysql',
     logging: isDevelopment, // ensure this is a boolean
 };
